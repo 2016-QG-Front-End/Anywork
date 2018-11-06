baeb3ea76920b7d0ddc969956520f5e5edb407cd
3216005169 <1151899508@qq.com>
2018-11-06 19:14:04 +0800

    okkk

--- src/utils/interaction.js
+++ src/utils/interaction.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
 // export const IP = 'http://10.21.56.107:8080/';  //http://10.21.48.11:8080
-export const IP = 'https://qgstudio.org/anywork/';  //http://10.21.48.11:8080
+// export const IP = 'https://qgstudio.org/anywork/';  //http://10.21.48.11:8080
 // export const IP = 'http://192.168.0.4:8080/';  //http://10.21.48.11:8080
+
+// export const IP = 'http://10.21.56.107:8080/';  //http://10.21.48.11:8080
+export const IP = '/anywork/'; //http://10.21.48.11:8080
+// export const IP = 'http://192.168.0.4:8080/';  //http://10.21.48.11:8080
+
 export const myAxios = axios.create({
-	baseURL: IP,
+  baseURL: IP,
 })
